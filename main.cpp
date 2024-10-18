af70b14668868cafa37bf03156e9e1257d38327b
daniiul <dmalakhov2005@gmail.com>
2024-10-17 18:46:14 -0700

    commenting code

--- main.cpp
+++ main.cpp
@@ -1,33 +1,53 @@
+// COMSC-210 | Midterm 1 | Daniil Malakhov
+// IDE used: Codeblocks
 #include <iostream>
 using namespace std;
 
+// creates 4 constant integer variables
 const int MIN_NR = 10, MAX_NR = 99, MIN_LS = 5, MAX_LS = 20;
 
+// class of Doubly Linked List
 class DoublyLinkedList {
 private:
+    // declares a private member structure called Node
+    // Node contains:
+    // an integer to represent the data stored by the node
+    // A pointer to a node structure to represent the previous node in the linked list
+    // A pointer to a node structure to represent the next node in the linked list
+    /* An overloaded declaration of node that allows the node structure to be declared with a specified value integer,
+        a pointer to a node structure representing previous node, and a pointer to a node structure representing next node.
+        the overloaded Node. Both pointer to node objects have default arguments of nullptr*/
     struct Node {
         int data;
         Node* prev;
         Node* next;
         Node(int val, Node* p = nullptr, Node* n = nullptr) {
-            data = val; 
+            data = val;
             prev = p;
             next = n;
         }
     };
 
+    // a pointer to node representing the head of the linked list
     Node* head;
+    // a pointer to node representing the tail of the linked list
     Node* tail;
 
 public:
+    // public default constructor that sets both head and tail pointers in Node to nullptr
     DoublyLinkedList() { head = nullptr; tail = nullptr; }
 
+    // inserts a node object after specified position and initializes it with given value
+    // arguments: integer representing value to insert into new node, integer representing where new node must be placed after given position
+    // returns: none
     void insert_after(int value, int position) {
+        // if the given position value is less than 0 (which is invalid) will print stating user error and returning/stopping the function
         if (position < 0) {
             cout << "Position must be >= 0." << endl;
             return;
         }
 
+        //Dynamically allocates memory for new Node object with given value and sets pointer of newNode to that object
         Node* newNode = new Node(value);
         if (!head) {
             head = tail = newNode;
@@ -57,21 +77,21 @@ public:
         if (!head) return;
 
         Node* temp = head;
-        
+
         while (temp && temp->data != value)
             temp = temp->next;
 
-        if (!temp) return; 
+        if (!temp) return;
 
         if (temp->prev)
             temp->prev->next = temp->next;
         else
-            head = temp->next; 
+            head = temp->next;
 
         if (temp->next)
             temp->next->prev = temp->prev;
         else
-            tail = temp->prev; 
+            tail = temp->prev;
 
         delete temp;
     }
@@ -81,14 +101,14 @@ public:
             cout << "List is empty." << endl;
             return;
         }
-    
+
         if (pos == 1) {
             pop_front();
             return;
         }
-    
+
         Node* temp = head;
-    
+
         for (int i = 1; i < pos; i++){
             if (!temp) {
                 cout << "Position doesn't exist." << endl;
@@ -101,12 +121,12 @@ public:
             cout << "Position doesn't exist." << endl;
             return;
         }
-    
+
         if (!temp->next) {
             pop_back();
             return;
         }
-    
+
         Node* tempPrev = temp->prev;
         tempPrev->next = temp->next;
         temp->next->prev = tempPrev;
@@ -123,7 +143,7 @@ public:
             tail = newNode;
         }
     }
-    
+
     void push_front(int v) {
         Node* newNode = new Node(v);
         if (!head)
@@ -134,7 +154,7 @@ public:
             head = newNode;
         }
     }
-    
+
     void pop_front() {
 
         if (!head) {
@@ -191,7 +211,7 @@ public:
 
     void print_reverse() {
         Node* current = tail;
-        if (!current) { 
+        if (!current) {
             cout << "List is empty." << endl;
             return;
         }
@@ -206,6 +226,6 @@ public:
 int main() {
     cout << MIN_NR + MIN_LS + MAX_NR + MAX_LS;  // dummy statement to avoid compiler warning
 
-    
+
     return 0;
 }
